6cfd2ac3dce1aa3e33c3f87d176de22ff4301de9
Md Shifut Hossain <mdshifut@gmail.com>
2020-10-02 11:09:37 +0600

    Added Formatted Message

--- src/component/PasswordChangeWithIntl.jsx
+++ src/component/PasswordChangeWithIntl.jsx
@@ -1,25 +1,34 @@
 import * as React from "react";
-import { injectIntl } from "react-intl";
+import { injectIntl, FormattedMessage } from "react-intl";
 
 class PasswordChangeWithIntl extends React.Component {
   render() {
     const { intl } = this.props;
     return (
-      <li>
-        <input
-          placeholder={intl.formatMessage({
-            defaultMessage: "New Password",
-            description: "placeholder text",
-          })}
-        />
-        <input
-          placeholder={intl.formatMessage({
-            id: "explicit-id",
-            defaultMessage: "Confirm Password",
-            description: "placeholder text",
-          })}
-        />
-      </li>
+      <>
+        <li>
+          <input
+            placeholder={intl.formatMessage({
+              defaultMessage: "New Password",
+              description: "placeholder text",
+            })}
+          />
+          <input
+            placeholder={intl.formatMessage({
+              id: "explicit-id",
+              defaultMessage: "Confirm Password",
+              description: "placeholder text",
+            })}
+          />
+        </li>
+        <li>
+          <FormattedMessage defaultMessage="Some demo text without id" />
+          <FormattedMessage
+            id="some-explicit"
+            defaultMessage="Some demo text with explicit id"
+          />
+        </li>
+      </>
     );
   }
 }
